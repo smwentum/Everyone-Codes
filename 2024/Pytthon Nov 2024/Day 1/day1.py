6414230995f9b907c7687307cb83935b87be8c02
smwentum <sai.wentum@hotmail.com>
2024-11-09 07:22:23 -0500

    refactor

--- 2024/Pytthon Nov 2024/Day 1/day1.py
+++ 2024/Pytthon Nov 2024/Day 1/day1.py
@@ -8,12 +8,19 @@ def partOne(fileName):
     count = 0
     with open(fileName) as f: 
         incommingCreatures = f.readline()
-        for creature in incommingCreatures:
+        count += getNumberOfPotionsNeeded(incommingCreatures)
+    print("The number of potions needed is:",count)
+
+def getNumberOfPotionsNeeded(incommingCreatrues):
+    count= 0
+    for creature in incommingCreatrues:
             match creature:
                 case 'B':
                     count += 1
                 case 'C':
                     count += 3
-    print("The number of potions needed is:",count)
-            
+                case 'D':
+                    count += 5
+    return count
+
 main()
