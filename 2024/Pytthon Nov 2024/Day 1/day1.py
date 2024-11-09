9f697206f590a971eea76167f85c433f60e0f54f
smwentum <sai.wentum@hotmail.com>
2024-11-09 07:38:55 -0500

    did part 2

--- 2024/Pytthon Nov 2024/Day 1/day1.py
+++ 2024/Pytthon Nov 2024/Day 1/day1.py
@@ -2,6 +2,7 @@
 def main(): 
 
     partOne("./part1Final.txt")
+    partTwo("./part2Final.txt")
 
 def partOne(fileName):
     #get the string 
@@ -9,6 +10,23 @@ def partOne(fileName):
     with open(fileName) as f: 
         incommingCreatures = f.readline()
         count += getNumberOfPotionsNeeded(incommingCreatures)
+    print("The number of potions needed is:",count)
+
+def partTwo(fileName):
+    count = 0
+    with open(fileName) as f: 
+        incommingCreatures = f.readline()
+
+        for i in range(0,len(incommingCreatures),2):
+            #print(incommingCreatures[i:i+2])
+            twoAtATime = incommingCreatures[i:i+2]
+            istwoMonster = True
+            for monster in twoAtATime: 
+                if monster == 'x':
+                    istwoMonster = False
+                count += getNumberOfPotionsNeeded(monster)
+            if istwoMonster:
+                count += 2
     print("The number of potions needed is:",count)
 
 def getNumberOfPotionsNeeded(incommingCreatrues):
